7292c4351927e24f39f26626760a8bfb568e810d
sam <sam.leider@hotmail.com>
2025-02-21 15:36:56 -0500

    Handle errors in invoice pages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,30 +21,39 @@ export async function createInvoice(formData: FormData) {
   const { customerId, amountInCents, status } = toInvoiceDTO(formData);
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
-
+  try {
+    await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+          `;
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amountInCents, status } = toInvoiceDTO(formData);
-
-  await sql`
+  try {
+    await sql`
   UPDATE invoices
   SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
   WHERE id = ${id}
   `;
-
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices");
 }
 
