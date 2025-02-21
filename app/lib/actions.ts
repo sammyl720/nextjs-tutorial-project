153e0469592e3d7564b1df4b318d2ab3ade91b61
sam <sam.leider@hotmail.com>
2025-02-21 15:14:12 -0500

    implement delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -43,6 +43,11 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
+
 function toInvoiceDTO(formData: FormData) {
   const dto = InvoiceDTO.parse({
     customerId: formData.get("customerId"),
